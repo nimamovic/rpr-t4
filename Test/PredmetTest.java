36a479e7085f832dfc6fbad2eca204905d617f71
nimamovic <nimamovic3@etf.unsa.ba>
2018-11-11 20:37:06 +0100

    poceti testovi

---
+++ Test/PredmetTest.java
@@ -0,0 +1,38 @@
+package ba.unsa.etf.rpr.tutorijal04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PredmetTest {
+
+    @org.junit.jupiter.api.Test
+    void getImePredmeta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setImePredmeta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getECTSBodovi() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setECTSBodovi() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isObavezan() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setObavezan() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void upisiStudenta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStudentiNaPredmetu() {
+    }
+}
